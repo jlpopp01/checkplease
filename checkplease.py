09e76aa321b2cc0bbee2b96c1014ea934f2fccb1
Jaye Popplewell <jlpopp01@gmail.com>
2020-05-13 18:45:32 -0400

    Text difference

--- checkplease.py
+++ checkplease.py
@@ -2,15 +2,15 @@ import math
 
 def split_check(total, number_of_people):
     if number_of_people <=1:
-        raise ValueError("More than 1 person is required to split the check")
+        raise ValueError("You can't split the check with yourself")
     return math.ceil(total / number_of_people)
 
-try: 
-    total_due = float(input("What is the total?  "))
-    number_of_people = int(input("How many people?  "))
+try:
+    total_due = float(input("How bad is it? Gimme the total  "))
+    number_of_people = int(input("How big is your crew?  "))
     amount_due = split_check(total_due, number_of_people)
-except ValueError as err: 
+except ValueError as err:
      print("Oh no! That's not a valid value, try again..")
      print("( {} )".format(err))
-else:   
+else:
     print("Each person owes ${}".format(amount_due))
